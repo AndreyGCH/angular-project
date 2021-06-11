821a3aeb36fb2c5958cb687c25f56b52a74c9c4c
AndreyGCH <guevaraandrey@gmail.com>
2021-06-11 10:38:12 -0600

    Setting up the Data Storage Service, Storing Recipes

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+    constructor(private http: HttpClient, private recipesService: RecipeService) { }
+
+    storeRecipes() {
+        const recipes = this.recipesService.getRecipes();
+        this.http
+            .put('https://ng-course-recipe-book-12d3f-default-rtdb.firebaseio.com/recipes.json',
+                recipes)
+            .subscribe(response => {
+                console.log(response);
+            });
+    }
+
+}
