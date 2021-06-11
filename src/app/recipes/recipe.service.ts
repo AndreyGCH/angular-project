eef4616e5099a513771be505c0d5a1204d72125a
AndreyGCH <guevaraandrey@gmail.com>
2021-06-11 10:54:16 -0600

    Transforming Response Data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,16 +8,17 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'Test', 'https://hips.hearstapps.com/hmg-prod/images/chicken-fajitas-horizontal-jpg-1522721531.jpg', [
-            new Ingredient('meat', 1),
-            new Ingredient('French Fries', 20),
-        ]),
-        new Recipe('Another 2 Test Recipe', 'Test', 'https://hips.hearstapps.com/hmg-prod/images/chicken-fajitas-horizontal-jpg-1522721531.jpg', [
-            new Ingredient('Buns', 2),
-            new Ingredient('Meat', 1),
-        ]),
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe('A Test Recipe', 'Test', 'https://hips.hearstapps.com/hmg-prod/images/chicken-fajitas-horizontal-jpg-1522721531.jpg', [
+    //         new Ingredient('meat', 1),
+    //         new Ingredient('French Fries', 20),
+    //     ]),
+    //     new Recipe('Another 2 Test Recipe', 'Test', 'https://hips.hearstapps.com/hmg-prod/images/chicken-fajitas-horizontal-jpg-1522721531.jpg', [
+    //         new Ingredient('Buns', 2),
+    //         new Ingredient('Meat', 1),
+    //     ]),
+    // ];
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) { }
 
